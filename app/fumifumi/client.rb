919849f533c3a23b05f89e19aa1537e2b6a0838c
mzp <mzpppp@gmail.com>
2017-02-20 08:44:46 +0900

    send request to fumifumi agent api

--- app/fumifumi/client.rb
+++ app/fumifumi/client.rb
@@ -4,6 +4,19 @@ require 'faraday_middleware'
 module Fumifumi
   class Client
     def upload(path)
+      response = client.post('magazines')
+    end
+
+    def client
+      @client ||= Faraday.new(
+        url: 'http://192.168.99.100:3000/api/agent',
+        headers: { user_agent: 'Fumifumi Rsync Agent' }
+      ) do |faraday|
+        faraday.request :multipart
+        faraday.response :json
+        faraday.response :logger
+        faraday.adapter  Faraday.default_adapter
+      end
     end
   end
 end
