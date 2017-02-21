120331b9106f3c064c89171f906724351c391e19
MIZUNO Hiroki <mzpppp@gmail.com>
2017-02-21 22:56:22 +0900

    Revert "Implement Fumifumi::Client#upload"

--- lib/fumifumi/response_error.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-module Fumifumi
-  class ResponseError < StandardError; end
-end
--- spec/lib/fumifumi/client_spec.rb
+++ spec/lib/fumifumi/client_spec.rb
@@ -4,10 +4,19 @@ require 'fumifumi/client'
 RSpec.describe Fumifumi::Client do
   describe '#upload' do
     let(:url) { 'http://192.168.99.100:3000/api/agent/magazines' }
-    let(:response) { { status: 403, body: '' } }
-    before { stub_request(:post, url).and_return(response) }
-
     let(:path) { Spec.root.join('fixtures', 'saint_oniisan.epub') }
-    it { expect { described_class.new.upload(path) }.to raise_error(Fumifumi::ResponseError) }
+
+    before { stub_request(:post, url).and_return(response) }
+    subject { described_class.new.upload(path) }
+
+    context '200 OK' do
+      let(:response) { { status: 200, body: '' } }
+      it { expect(subject).to be_truthy }
+    end
+
+    xcontext '403 Forbidden' do
+      let(:response) { { status: 403, body: '' } }
+      it { expect { subject }.to raise_error }
+    end
   end
 end
