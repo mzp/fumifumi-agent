be2c84361e1973060b18937ad73d47a214983064
mzp <mzpppp@gmail.com>
2017-02-22 20:27:54 +0900

    :lipstick: fix spec

--- spec/lib/fumifumi/api/exist_spec.rb
+++ spec/lib/fumifumi/api/exist_spec.rb
@@ -2,12 +2,12 @@
 require 'fumifumi'
 
 RSpec.describe Fumifumi::Api::Exist do
-  let(:url) { 'http://192.168.99.100:3000/api/agent/magazines/exists?title=BookTitle' }
+  let(:url) { 'http://192.168.99.100:3000/api/agent/magazines/exists?filename=foo.epub' }
   before { stub_request(:get, url).and_return(response) }
 
   let(:client) { Fumifumi::Client.build_client }
-  let(:title) { 'BookTitle' }
-  subject { described_class.new(client).call(title) }
+  let(:filename) { 'foo.epub' }
+  subject { described_class.new(client).call(filename) }
 
   context 'exist' do
     let(:response) { { status: 200, body: 'true' } }
